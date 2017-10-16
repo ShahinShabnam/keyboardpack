5c5eb14467179cdc3cf21702c5a5462ae88e6c32
ShahinShabnam <shabnam.shahin@rancelab.com>
2017-10-16 14:06:23 +0530

    last

--- keypack.umd.js
+++ keypack.umd.js
@@ -241,14 +241,14 @@ var CustomKeyboardComponent = (function () {
      */
     CustomKeyboardComponent.prototype.ngOnInit = function () {
         var _this = this;
-        this.subscriptions = this.customKeyboardService.filterOn('input:type:change').subscribe(function (d) {
-            alert(d.data + "components");
-            _this.inputType = d.data;
-        });
         this.inputstr = "";
         this.CapsLock = false;
         // this.keys = ["Esc", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "bksp", "Caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "Enter", "<--", "z", "x", "c", "v", "b", "n", "m", "-", "-->", "Spacebar", "0",];
         this.caretPos = 0;
+        this.subscriptions = this.customKeyboardService.filterOn('input:type:change').subscribe(function (d) {
+            alert(d.data + "components");
+            _this.inputType = d.data;
+        });
     };
     /**
      * @param {?} event
