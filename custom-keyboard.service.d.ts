1a3d64b8d780ea5711d60c12975c179883e93579
ShahinShabnam <shabnam.shahin@rancelab.com>
2017-10-16 12:38:25 +0530

    first

--- custom-keyboard.service.d.ts
+++ custom-keyboard.service.d.ts
@@ -3,13 +3,11 @@ import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
-import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/filter';
+import { Subject } from 'rxjs/Subject';
 export declare class CustomKeyboardService {
     private _http;
     subject: Subject<any>;
-    response: any;
-    inputType: any;
     constructor(_http: Http);
     passvalue(passvalue: string): void;
     filterOn(id: string): Observable<any>;
